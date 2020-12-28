9d986d6f5579ec6180d77ebccb38055a76a91282
GithubTiger <58816240+karol71927@users.noreply.github.com>
2020-12-28 16:21:41 +0100

    Delete async.handler.js

--- src/api/async.handler.js
+++
@@ -1,7 +0,0 @@
-const asyncHandler = (callback) => {    //zajmuje sie bledami i nie potrzeba wtedy bloku try catch
-    return function(req, res, next){
-        callback(req, res, next).catch(next);
-    }
-}
-
-module.exports = asyncHandler;
