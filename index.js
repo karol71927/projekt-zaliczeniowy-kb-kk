c7bb3c4d7d9de9604bd016112585885ef0e972c2
Kinga <58816347+dazaiii@users.noreply.github.com>
2020-12-30 19:58:56 +0100

    index.js update

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const api = require('./src/api');
+const cookieParser = require('cookie-parser');
+const jwt = require("jsonwebtoken");
+const errorHandler = require("./src/middlewares/errorHandler");
+const databaseErrorHandler = require("./src/middlewares/databaseErrorHandler");
+const config = require('./config');
 
-const port = 9000;
+const port = process.env.PORT || 9000;
 const env = process.env.NODE_ENV || 'development';
 const app = express();
-
+app.use(express.json());
+app.use(cookieParser(config.cookiesSecret));
+app.use('/api', api);
+app.use(databaseErrorHandler);
+app.use(errorHandler);
 app.get('/', (req, res) => {
     res.send({msg:'Witam'})
 })
